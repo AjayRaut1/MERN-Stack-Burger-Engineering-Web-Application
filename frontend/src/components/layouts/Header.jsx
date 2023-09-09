0da0ebc26fbec86c5795f5f3b0bdfa95048d987c
AjayRaut1 <ajauraut2002@gmail.com>
2023-09-09 11:52:40 +0530

    Created Header Layout without CSS

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import {FiShoppingCart,FiLogIn} from "react-icons/fi";
+import {FaUser} from "react-icons/fa";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({isAuthenticated = false}) => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <IoFastFoodOutline />
+        </div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/cart">
+            <FiShoppingCart />
+          </Link>
+          <Link to={isAuthenticated? "/me" : "/login"}>
+            {isAuthenticated ? <FaUser /> : <FiLogIn />}
+          </Link>
+        </div>
+
+      </div>
+    </nav>
+  );
 };
 
 export default Header;

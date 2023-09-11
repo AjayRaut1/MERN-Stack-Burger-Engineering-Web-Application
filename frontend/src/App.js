d9d0fb3818c016e831e273df19ccbababac69787
AjayRaut1 <ajauraut2002@gmail.com>
2023-09-11 20:00:14 +0530

    Shipping Component Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home"
 import Header from "./components/layouts/Header"
 import Contact from "./components/contact/Contact"
 import Cart from "./components/cart/Cart"
+import Shipping from "./components/cart/Shipping"
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -12,6 +13,7 @@ import "./styles/menu.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
 import "./styles/cart.scss";
+import "./styles/shipping.scss";
 
 function App() {
   return <Router>
@@ -20,6 +22,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/shipping" element={<Shipping />} />
     </Routes>
     
   </Router>
